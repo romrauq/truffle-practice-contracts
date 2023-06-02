b79312b8489db08ddcd3a88a3071e8c1e6bd10a8
John Quarmor <johnequarmor@gmail.com>
2023-06-02 12:50:15 +0000

    Created solidity file containing contract: `StorageContract`

---
+++ contracts/01_StorageContract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract StorageContract {
+	uint value;
+
+	constructor(uint8 _value{
+		value = _value;
+	} 
+
+	function getValue() public view returns (uint) {
+		return value;
+	}
+
+	function setValue(uint _new_value) public {
+		value = _new_value;
+	} 
+}
+ 
